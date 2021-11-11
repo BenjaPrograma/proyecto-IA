cc82ca155675fd7e499c5e648dcb72568f6f5d24
Benjamín Yon Bazán <bayon@uc.cl>
2021-11-11 05:40:35 -0300

    major fix

--- r2r_src/agent.py
+++ r2r_src/agent.py
@@ -777,8 +777,8 @@ class Seq2SeqAgent(BaseAgent):
 
                     mix_ctx = torch.stack(mix_ctx).cuda()
                     #print("MIX SHAPE",mix_ctx.shape)
-                    vl_pair = torch.cat((h1,mix_ctx), dim=1)
-                    prob = self.matching_network(vl_pair)
+                    #vl_pair = torch.cat((h1,mix_ctx), dim=1)
+                    prob = self.matching_network(h1, mix_ctx)
                     #print("PROB SHAPE", prob.shape)
                     #prob = prob.select(0,0)
 
